de4ef8584a1fe57a871212dc67af32e480303d15
Carla <carlawarda@gmail.com>
2023-11-22 16:35:56 -0800

    submission

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,11 +1,48 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  let found = accounts.find((account) => account.id === id);
+    if (found) return found; 
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  let alphabetized = accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
+  return alphabetized; 
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  let countedBooks = books.reduce((countState, book) => {
+    let count = book.borrows.reduce((borrowState, borrow) => {
+     if (borrow.id === account.id) {
+       return borrowState += 1
+     }
+     return borrowState
+    }, 0)
+ 
+    return countState += count
+  }, 0)
+ 
+   return countedBooks
+ }
 
-function getBooksPossessedByAccount(account, books, authors) {}
+//reduce 
 
+//It returns the number of times the account's ID appears in any book's `borrows` array
+
+function getBooksPossessedByAccount(account, books, authors) {
+  let mappedBooks = books.map(book => {
+    const author = authors.find(author => author.id === book.authorId)
+    return {
+      ...book,
+      author
+    }
+  })
+  return mappedBooks.filter((book) => book.borrows.some(borrow => borrow.id === account.id && borrow.returned === false))
+}
+
+//if books.id.returned === true result.push[books]
+
+
+//It returns an array of book objects, including author information, that represents all books _currently checked out_ by the given account. _Look carefully at the object below,_ as it's not just the book object; the author object is nested inside of it.
+//if book is checked out return books and author info 
 module.exports = {
   findAccountById,
   sortAccountsByLastName,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,33 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  const found = authors.find((author) => author.id === id)
+  if (found) return found 
+} // passed tests
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  const found = books.find((book) => book.id === id)
+  if (found) return found 
+} // passed tests
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const checkedOut = books.filter((book) => book.borrows[0].returned === false)
+  const returned =  books.filter((book) => book.borrows[0].returned === true)
+  return [checkedOut, returned]
+} // passed tests
 
-function getBorrowersForBook(book, accounts) {}
+// declare function that takes in two perameters
+// set empty array variable
+// loop through books checking for borrows
+// if returned push 
+function getBorrowersForBook(book, accounts) {
+  const borrowersMap = book.borrows.map((borrow) => {
+    const account = accounts.find(account => account.id === borrow.id) 
+    return {
+      ...account,
+      returned: borrow.returned
+    }
+  })
+  return borrowersMap.slice(0, 10)
+}
 
 module.exports = {
   findAuthorById,
